d71dfe60dafcf50e588e51eec875cd59f63ff46c
Nestor <nestor@nstlopez.com>
2024-01-26 19:19:11 +0100

    Add env vars and logs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use dotenv::dotenv;
+use log::{info, warn};
+use std::{env, io};
 
 async fn get_ip(req: HttpRequest) -> impl Responder {
-    let ip = req
-        .peer_addr()
-        .map(|addr| format!("{}\n", addr.ip()))
-        .unwrap_or_else(|| "Unknown".to_string());
-    ip
+    if let Some(addr) = req.peer_addr() {
+        info!("Received request from {}", addr);
+        format!("{}\n", addr.ip())
+    } else {
+        warn!("Could not get client IP");
+        "Unknown\n".to_string()
+    }
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let bind_address = format!("0.0.0.0:{}", port);
+
+    info!("Starting server at {}", &bind_address);
+
     HttpServer::new(|| App::new().route("/", web::get().to(get_ip)))
-        .bind("0.0.0.0:3000")?
+        .bind(&bind_address)?
         .run()
         .await
 }
