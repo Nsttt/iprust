c47312b14a127c48023b8500f308a2aff9e7fe1e
Nestor <nestor@nstlopez.com>
2024-01-26 18:50:52 +0100

    Move to use actix and add code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn get_ip(req: HttpRequest) -> impl Responder {
+    let ip = req
+        .peer_addr()
+        .map(|addr| format!("{}\n", addr.ip()))
+        .unwrap_or_else(|| "Unknown".to_string());
+    ip
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(get_ip)))
+        .bind("0.0.0.0:3000")?
+        .run()
+        .await
 }
